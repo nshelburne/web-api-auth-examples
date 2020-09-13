b390c2ff7a0c34abe4c5853d0748a6025bf20f4b
nshelburne <nsnadia12@gmail.com>
2020-09-13 18:59:38 -0400

    Update app.js

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '8ae510538fb74b1587054e72f4ad107a'; // Your client id
+var client_secret = 'Orangessuck'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
